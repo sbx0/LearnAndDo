f90c16dd37a663f572ac96d91437792f0ebf329c
sbx0 <jsonsnownothing@gmail.com>
2020-03-01 21:04:21 +0800

    没有可见性

---
+++ src/volatile/VolatileDemo.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
+class DataStorage {
+    int number = 0;
+
+    public void to100() {
+        this.number = 100;
+    }
+}
+
+public class VolatileDemo {
+    public static void main(String[] args) {
+        DataStorage dataStorage = new DataStorage();
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t is start.");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dataStorage.to100();
+            System.out.println(Thread.currentThread().getName() + "\t update number value = " + dataStorage.number);
+        }, "thread-no-1").start();
+        while (dataStorage.number == 0) {}
+        System.out.println(Thread.currentThread().getName() + "\t mian end.");
+    }
+}
